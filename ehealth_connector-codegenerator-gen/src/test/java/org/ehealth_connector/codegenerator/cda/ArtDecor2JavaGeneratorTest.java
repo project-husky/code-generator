d0557afc8e19ebfb771ad011252aaed97e24a3e0
Tony Schaller <tony.schaller@medshare.net>
2019-08-07 10:00:12 +0200

    Disabling Code Generator Test

--- ehealth_connector-codegenerator-gen/src/test/java/org/ehealth_connector/codegenerator/cda/ArtDecor2JavaGeneratorTest.java
+++ ehealth_connector-codegenerator-gen/src/test/java/org/ehealth_connector/codegenerator/cda/ArtDecor2JavaGeneratorTest.java
@@ -28,7 +28,6 @@ import org.apache.commons.io.FileUtils;
 import org.ehealth_connector.codegenerator.java.JavaCodeGenerator;
 import org.ehealth_connector.common.utils.FileUtil;
 import org.ehealth_connector.common.utils.Util;
-import org.junit.Test;
 
 import net.sf.saxon.s9api.SaxonApiException;
 
@@ -166,7 +165,7 @@ public class ArtDecor2JavaGeneratorTest {
 	 */
 	// This is for debugging purposes, only. Do not commit this test with the
 	// test annotation enabled!
-	@Test
+	// @Test
 	public void doLrepTest()
 			throws SaxonApiException, IOException, JAXBException, ClassNotFoundException,
 			IllegalAccessException, IllegalArgumentException, InvocationTargetException,
